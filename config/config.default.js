221126ebfb825db67e192904fa2c93b33ece7620
johnnynode <johnnynode@gmail.com>
2020-02-02 09:05:53 +0800

    add: session conf

--- config/config.default.js
+++ config/config.default.js
@@ -15,6 +15,15 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1589092851067_1375';
 
+  // session conf
+  config.session = {
+    key: 'SESSION_ID',
+    maxAge: 864000,
+    httpOnly: true,
+    encrypt: true,
+    renew: true, //  延长会话有效期
+  };
+
   // add your middleware config here
   config.middleware = [];
 
