d2e7531f5a7078522835e758e9de1937364b6504
johnnynode <johnnynode@gmail.com>
2020-02-11 10:13:54 +0800

    update: admin access

--- app/controller/admin/access.js
+++ app/controller/admin/access.js
@@ -50,8 +50,29 @@ class AccessController extends BaseController {
     access.save();
     await this.success('/admin/access', '增加权限成功');
   }
+
   async edit() {
-    await this.ctx.render('admin/access/edit');
+    const id = this.ctx.request.query.id;
+    // 获取编辑的数据
+    const accessResult = await this.ctx.model.Access.find({ _id: id });
+    const result = await this.ctx.model.Access.find({ module_id: '0' });
+    await this.ctx.render('admin/access/edit', {
+      list: accessResult[0],
+      moduleList: result,
+    });
+  }
+
+  async doEdit() {
+    // console.log(this.ctx.request.body);
+    const updateResult = this.ctx.request.body;
+    const id = updateResult.id;
+    const module_id = updateResult.module_id;
+    // 菜单  或者操作
+    if (module_id !== '0') {
+      updateResult.module_id = this.app.mongoose.Types.ObjectId(module_id); // 调用mongoose里面的方法把字符串转换成ObjectId
+    }
+    await this.ctx.model.Access.updateOne({ _id: id }, updateResult);
+    await this.success('/admin/access', '修改权限成功');
   }
 }
 module.exports = AccessController;
