52c5ed5b9f3a9707ba7d2554f0e197cc6cb2c926
johnnynode <johnnynode@gmail.com>
2020-08-12 12:24:06 +0800

    update: admin goods type attr

--- app/model/goods_type_attribute.js
+++ app/model/goods_type_attribute.js
@@ -11,6 +11,7 @@ module.exports = app => {
         attr_type: { type: String }, // 类型  1 input    2  textarea    3、select
         attr_value: { type: String }, // 默认值： input  textarea默认值是空     select框有默认值  多个默认值以回车隔开
         status: { type: Number, default: 1 },
+        sort: { type: Number, default: 100 },
         add_time: {
             type: Number,
             default: d.getTime(),
